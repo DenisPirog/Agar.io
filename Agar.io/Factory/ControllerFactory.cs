607ddc9d8e792fc5a99a2e54d273b84a12a0c1a2
Denis <denispirog878@gmail.com>
2022-06-18 18:15:52 +0300

    Fix Copy-Pasta

--- Agar.io/Factory/ControllerFactory.cs
+++ Agar.io/Factory/ControllerFactory.cs
@@ -4,22 +4,32 @@ using SFML.System;
 
 namespace Agar.io.Factory
 {
+    enum ControllerType
+    {
+        Human,
+        AI,
+    }
+
     class ControllerFactory
     {
         public static PlayerController CreatePlayer()
-        {
-            int defaultRadius = 30;
-            Vector2f target = Generator.GetPositionOnGameField(defaultRadius);
-
-            return new PlayerController(target);
-        }
+            => (PlayerController) CreateController(ControllerType.Human);
 
         public static BotController CreateBot()
+            => (BotController)CreateController(ControllerType.AI);
+
+        private static Controller CreateController(ControllerType controllerType)
         {
             int defaultRadius = 30;
             Vector2f target = Generator.GetPositionOnGameField(defaultRadius);
 
-            return new BotController(target);
+            switch (controllerType)
+            {
+                case ControllerType.Human : return new PlayerController(target);
+                case ControllerType.AI: return new BotController(target);
+            }
+
+            throw null;
         }
     }
 }
